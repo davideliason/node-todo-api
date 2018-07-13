068cfcaa071b41921b35c3cfd20116b0c3048d75
David Eliason <david@davethemaker.com>
2018-07-13 14:30:05 -0700

    upgraded mongodb version from 2* to 3.1.1 an refactored db connection accordingly

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,12 +1,23 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
     // handle errors, return if there is one
     if (err) {
         return console.log('unable to connect to MongoDB server');
 
     }
     console.log("connected to MongoDB server");
+    const db = client.db("TodoApp");
 
-    db.close();
+    db.collection('Todos').insertOne({
+        text: 'Something to do',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log("unable to insert todo", err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2))
+    });
+
+    client.close();
 });
