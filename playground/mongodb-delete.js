9b0425f28738369a2693e90c97fc64c479bcf232
David Eliason <david@davethemaker.com>
2018-07-14 08:40:30 -0700

    successfully find duplicate name properties and delete them

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -1,5 +1,7 @@
 // const MongoClient = require('mongodb').MongoClient;
 const { MongoClient, ObjectID } = require('mongodb');
+var obj = new ObjectID();
+
 
 MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true }, (err, client) => {
     // handle errors, return if there is one
@@ -12,23 +14,42 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
 
     // deleteMany
 
-    db.collection("Todos").deleteMany({ text: "drink more coffee" }).then((result) => {
-        console.log(result);
-    });
-    // deleteOne
-    db.collection("Todos").deleteOne({
-        text: "walk the dog"
-    }).then((result) => {
-        console.log("document deleted", result);
-    });
+    // db.collection("Todos").deleteMany({ text: "drink more coffee" }).then((result) => {
+    //     console.log(result);
+    // });
+    // // deleteOne
+    // db.collection("Todos").deleteOne({
+    //     text: "walk the dog"
+    // }).then((result) => {
+    //     console.log("document deleted", result);
+    // });
 
     // deleteOne example two
-    db.collection("Todos").deleteOne({
-        text: "sleep"
+    // db.collection("Todos").deleteOne({
+    //     text: "sleep"
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+    // findOneAndDelete
+    // db.collection("Todos").findOneAndDelete({
+    //     completed: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // practice solution of removing duplicates
+    db.collection("Users").deleteMany({
+        name: "Tom"
     }).then((result) => {
         console.log(result);
     });
-    // findOneAndDelete
+
+    // db.collection("Users").findOneAndDelete({
+    //     _id: new ObjectID('5b49cdbd4d1f0eb3645bead4')
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
 
     // db.close();
 });
