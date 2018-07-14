9821abf122485754a8269e266782e94c28427eb5
David Eliason <david@davethemaker.com>
2018-07-14 09:43:43 -0700

    successfully used findOneAndUpdate method to find a document and update the name field as well as using the update operator  to increment age value. Returned new document

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -14,20 +14,39 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true
 
     // findOneAndUpdate
 
-    db.collection("Todos").findOneAndUpdate({
+    // db.collection("Todos").findOneAndUpdate({
 
-        _id: new ObjectID("5b49d68b4d1f0eb3645bed4b")
+    //     _id: new ObjectID("5b49d68b4d1f0eb3645bed4b")
 
+    // }, {
+    //         $set: {
+    //             completed: true
+    //         }
+    //     }, { returnOrignal: false }).then((result) => {
+    //         if (err) {
+    //             console.log("unable to find document and delete", err);
+    //         }
+    //         console.log("success", result);
+    //     });
+
+    // exercise in finding a document by _id, updating the name field 
+    // with a new value, and using the $inc operator to increment the age
+    // field by one value
+
+    db.collection("Users").findOneAndUpdate({
+        _id: new ObjectID("5b49cdd14d1f0eb3645beada")
     }, {
             $set: {
-                completed: true
+                name: "David"
+            },
+            $inc: {
+                age: 1
             }
-        }, { returnOrignal: false }).then((result) => {
-            if (err) {
-                console.log("unable to find document and delete", err);
-            }
+        },
+        {
+            returnOriginal: false
+        }).then((result) => {
             console.log("success", result);
         });
-
     // db.close();
 });
