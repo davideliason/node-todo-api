102f37b86b71ea43ad79e235da7ca05c04ebd61a
David Eliason <david@davethemaker.com>
2018-07-16 15:42:57 -0700

    git rid of unecessary todo document instance, add User model with validators

--- server/server.js
+++ server/server.js
@@ -3,33 +3,34 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/ToDoApp");
 
+var User = mongoose.model({
+    email: {
+        type: string,
+        required: true,
+        trim: true,
+        minlength: 1
+    }
+})
+
 var Todo = mongoose.model('Todo', {
     text: {
         type: String,
-        required: true
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
         type: Boolean
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 var newTodo = new Todo({
     text: "Make coffee"
-});
-
-var newTodo2 = new Todo({
-    text: "drink coffee",
-    completed: true,
-    completedAt: new Date()
-});
-
-newTodo2.save().then((doc) => {
-    console.log("saved", JSON.stringify(doc, undefined, 2));
-}, (e) => {
-    console.log("error saving doc");
 });
 
 newTodo.save().then((doc) => {
