b9cba25ef84a656a3c3608fd68e1c94ddd9799e9
David Eliason <david@davethemaker.com>
2018-07-17 08:46:13 -0700

    add queries within playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,25 +1,48 @@
+const { ObjectID } = require('mongodb');
+
 const { mongoose } = require('./../server/db/mongoose');
-const { Todo } = require('./../server/models/Todo');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/user')
+
+var id = '5b4d23b624f9833770f4f2cf';
+
+User.findById(id).then((user) => {
+    if (!user) {
+        return console.log("user not found");
+    }
+    console.log("user :", user);
+}).catch((e) => console.log(e));
+
+
+
+
+// query User collection and get an id
+
 
 // use a db collection document id for queries
-var id = '5b4cb94dcf398d204d4d1d91';
+// var id = '5b4cb94dcf398d204d4d1d91';
 
-// mongoose will translate string id to ObjectID
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log("Todos", todos);
-});
+// if (!ObjectID.isValid(id){
+//     console.log("id not valid");
+// });
 
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log("Todo", todo);
-});
+// // mongoose will translate string id to ObjectID
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log("Todos", todos);
+// });
 
-Todo.findById(id).then((todo) => {
-    if (!todo) {
-        console.log("id not found ")
-    }
-    console.log("Todo by id: ", todo);
-});
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log("Todo", todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('id not found');
+//     }
+//     console.log("todo by id", todo);
+// }).catch((e) => console.log(e));
+
