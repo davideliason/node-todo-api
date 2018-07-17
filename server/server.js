1df1304c74e1ec798924d6d2b3eb655526973611
David Eliason <david@davethemaker.com>
2018-07-17 11:36:12 -0700

    successfully query using findById using parsed req.params returning document:

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -9,36 +10,45 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// middleware config
-// bodyParser will take the JSON string and turn it into an object
-// .. attaching it to the req object
-
 app.post('/todos', (req, res) => {
-    // print out the body compliments of bodyParser
     var todo = new Todo({
         text: req.body.text
     });
 
     todo.save().then((doc) => {
-        res.send(doc)
+        res.send(doc);
     }, (e) => {
-        console.log("unable to retrieve post");
         res.status(400).send(e);
     });
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({
-            todos
-        });
+        res.send({ todos });
     }, (e) => {
-        res.status(400, e);
-    })
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    // validate id
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    // if id is valid...
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send(todo);
+    }).catch((e) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
-    console.log("app started on port 3000");
-})
+    console.log('Started on port 3000');
+});
 
 module.exports = { app };
