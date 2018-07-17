1d1c038b3faf69c71c64bdb72c574af1c10386fa
David Eliason <david@davethemaker.com>
2018-07-16 19:19:43 -0700

    created a POST route handler within server.js which used body-parser to parse the JSON string body provided by the user, in this case using POSTMAN. Used a Promise to handle the binary pathway, success leading to a response with a copy of the sent data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
 
-// grabbing the return object from mongoose.js
-// use ES6 
 var { mongoose } = require('./db/mongoose');
-
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+// middleware config
+// bodyParser will take the JSON string and turn it into an object
+// .. attaching it to the req object
+
+app.post('/todos', (req, res) => {
+    // print out the body compliments of bodyParser
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc)
+    }, (e) => {
+        console.log("unable to retrieve post");
+        res.status(400).send(e);
+    });
+});
+
+app.listen(3000, () => {
+    console.log("app started on port 3000");
+})
