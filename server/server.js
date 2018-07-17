e6e57a91f47811ec862bdba8ace92cf41b14e15c
David Eliason <david@davethemaker.com>
2018-07-16 17:28:55 -0700

    refactor so that all code related to model creation is put within out modules and imported within server.js

--- server/server.js
+++ server/server.js
@@ -1,50 +1,7 @@
 
 // grabbing the return object from mongoose.js
+// use ES6 
 var { mongoose } = require('./db/mongoose');
 
-var User = mongoose.model('User', {
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 1
-    }
-});
-
-var tom = new User({
-    email: "this@chateau.com"
-});
-
-tom.save().then((doc) => {
-    console.log("new user saved", JSON.stringify(doc, undefined, 2));
-
-}, (e) => {
-    console.log("error", e);
-});
-
-var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        required: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-        type: Number,
-        default: null
-    }
-});
-
-var newTodo = new Todo({
-    text: "Make coffee"
-});
-
-newTodo.save().then((doc) => {
-    console.log("doc saved", doc);
-}, (e) => {
-    console.log("unable to save document");
-});
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
