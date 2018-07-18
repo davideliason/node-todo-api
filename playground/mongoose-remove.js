926d20a51fe043b8ac6403cf4b16acd6d76ffdd8
David Eliason <david@davethemaker.com>
2018-07-17 18:39:43 -0700

    add second remove method

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -7,3 +7,7 @@ const { User } = require('./../server/models/user')
 Todo.findByIdAndRemove('5b4df1f1792c73566457504e').then((todo) => {
     console.log("removed", todo);
 });
+
+Todo.findOneAndRemove({ _id: '5b4d519959d131479c5e35d0' }).then((todo) => {
+    console.log('removed', todo);
+});
