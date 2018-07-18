960b87a27df7c92555439b2cdb6853b03205f253
David Eliason <david@davethemaker.com>
2018-07-17 19:05:31 -0700

    add delete route using findByIdAndRemove

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/ToDoApp");
 
-
-
 module.exports = mongoose;
 
 
--- server/server.js
+++ server/server.js
@@ -40,7 +40,24 @@ app.get('/todos/:id', (req, res) => {
     // okay it's valid
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            res.status(400).send();
+            return res.status(400).send();
+        }
+        res.status(200).send(todo);
+    }).catch((err) => {
+        res.status(404).send();
+    });
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    // id is valid:
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
         }
         res.status(200).send(todo);
     }).catch((err) => {
